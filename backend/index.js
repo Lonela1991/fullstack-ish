bb98323a4def8b5d1969cb2abda858449e249901
Louise Nensen Lagnefors <nensen-lagnefors@hotmail.com>
2025-10-23 00:36:10 +0200

    Implement fullstack update: fetch todos from database via API and display in React frontend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 import express from 'express';
 import path from 'path';
+import dotenv from 'dotenv';
+import {Client} from 'pg';
+
+dotenv.config();
+
+const pgClient = new Client({
+  connectionString: process.env.PGURI,
+});
+
+pgClient.connect()
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 // "/api" istället för enbart "/" för att undvika krock med frontend-rutter
-app.get('/api', (_request, response) => {
-  response.send({ hejsan: 123 })
+app.get('/api', async (_request, response) => {
+  const {rows} = await pgClient.query('SELECT * FROM todos;');
+  response.send(rows);
 })
 
 console.log("Katalogen vi är i:",path.resolve());
